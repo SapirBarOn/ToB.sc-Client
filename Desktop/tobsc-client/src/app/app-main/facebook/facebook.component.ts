ed23707a86ca39315915971e98c493c93011db71
shiran44 <shirant442@gmail.com>
2018-05-13 10:44:57 +0300

    Update facebook.component.ts

--- Desktop/tobsc-client/src/app/app-main/facebook/facebook.component.ts
+++ Desktop/tobsc-client/src/app/app-main/facebook/facebook.component.ts
@@ -28,7 +28,7 @@ export class FacebookComponent implements OnInit {
     this.socialAuthService.signIn(socialPlatformProvider).then(
       (userData) => {
         console.log(socialPlatform+" sign in data : " , userData);
-                  console.log(a.name);
+                  console.log("test name",name);
         // Now sign-in with userData
             
       }
